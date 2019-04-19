e20e0f90155cb0f349e0405f9cf0a232e33e54e8
Adithi Krishnan <38013049+adithikrishnan@users.noreply.github.com>
2019-04-19 21:17:45 +0530

    implemented newView() and bindView() methods

--- PetsApp/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ PetsApp/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -5,6 +5,9 @@ import android.database.Cursor;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import com.example.android.pets.data.PetContract;
+import com.example.android.pets.data.PetContract.PetEntry;
 
 public class PetCursorAdapter extends CursorAdapter {
 
@@ -14,12 +17,21 @@ public class PetCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        // TODO: Fill out this method and return the list item view (instead of null)
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // TODO: Fill out this method
+        TextView nameTextView = view.findViewById(R.id.name);
+        TextView summaryTextView = view.findViewById(R.id.summary);
+        
+        int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
+        int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+        
+        String petName = cursor.getString(nameColumnIndex);
+        String petBreed = cursor.getString(breedColumnIndex);
+        
+        nameTextView.setText(petName);
+        summaryTextView.setTextt(petBreed);
     }
 }
