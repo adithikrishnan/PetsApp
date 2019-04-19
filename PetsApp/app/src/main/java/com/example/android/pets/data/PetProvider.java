89b41abf1843d4b9d915d1abde8892674c760df7
Adithi Krishnan <38013049+adithikrishnan@users.noreply.github.com>
2019-04-19 14:24:41 +0530

    Added UriMatcher methods

--- PetsApp/app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ PetsApp/app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -4,9 +4,19 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import com.example.android.pets.data.PetContract;
 
 public class PetProvider extends ContentProvider{
   private PetDbHelper mDbHelper;
+  private static final int PETS = 100;
+  private static final int PET_ID = 101;
+  private static final UriMatcher sUriMatcher = new UriMatcher(UriMatchedr.NO_MATCH);
+  
+  static {
+    sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+    sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
+  }
+  
   
   @Override
   public boolean onCreate(){
